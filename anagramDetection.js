b7aeabd25f2d98924a2678f2ec8c2509dd4598aa
Standy Merizier <standymerizier@Standys-MacBook-Pro.local>
2018-07-25 18:42:52 -0400

    update

---
+++ anagramDetection.js
@@ -0,0 +1,23 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+//
+// Note: anagrams are case insensitive
+//
+// Complete the function to return true if the two arguments given are anagrams of theeach other; return false otherwise.
+//
+// Examples
+// "foefet" is an anagram of "toffee"
+//
+// "Buckethead" is an anagram of "DeathCubeK"
+
+
+
+
+
+var isAnagram = function(test, original) {
+  if(test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")){
+    return true
+  }
+  else{
+    return false
+  }
+};
