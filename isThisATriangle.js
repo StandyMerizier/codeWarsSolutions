4da460732a7eb9d7ab60ed22ef29df4a07c52690
Standy Merizier <standymerizier@Standys-MacBook-Pro.local>
2018-08-01 21:15:08 -0400

    update

---
+++ isThisATriangle.js
@@ -0,0 +1,17 @@
+// Is This A Triangle? Question
+//
+// Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+//
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+
+
+
+function isTriangle(a,b,c){
+  if ((a+b<=c) || (a+c<=b) || (b+c<=a)) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
