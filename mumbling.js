16e0cacb0e8cb2f43a441e750f194bca84e658cf
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2018-12-15 22:29:02 -0500

    Create mumbling.js

---
+++ mumbling.js
@@ -0,0 +1,32 @@
+//This time no story, no theory. The examples below show you how to write function accum:
+//
+//Examples:
+//
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+//The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+//solution
+function accum(s) {
+  let mumble = ''
+  let arr = s.toUpperCase().split('')
+  let count = 0
+  for(let i = 0; i<arr.length; i++){
+    if(i<arr.length - 1){
+      mumble += arr[i] + arr[i].repeat(count).toLowerCase() + '-'
+      count++
+    }
+    else{
+      mumble += arr[i] + arr[i].repeat(count).toLowerCase()
+    }
+  }
+  return mumble
+}
+
+
+//another solution
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
