54624b7e3633fe05e8e29810d2dda6d3e3559c5a
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2019-03-18 12:56:53 -0400

    Create addingUsefulFunctionalityToJavaScriptArrays.js

---
+++ addingUsefulFunctionalityToJavaScriptArrays.js
@@ -0,0 +1,45 @@
+//The JavaScript standard now includes functional additions to array like map, filter and reduce, but sadly is missing the convenience functions range and sum . Implement a version of range and sum (which you can then copy and use in your future Kata to make them smaller).
+//
+//Array.range(start, count) should return an array containing 'count' numbers from 'start' to 'start + count' Example: Array.range(0, 3) returns [0, 1, 2]
+//
+//Array.sum() return the sum of all numbers in the array Example: [0, 1, 2].sum() returns 3 Example: Array.range(-1,4).sum() should return 2
+//
+//While not forbidden try to write both function without using a for loop
+
+
+//solution
+Array.range = function(start, count) {
+  if(count !== 0){
+    let array = [start]
+    let next = start
+    let stop = 1
+    while(stop < count){
+      next = next+1
+      array.push(next)
+      stop = stop + 1
+    }
+    return array;
+  }
+  return []
+}
+
+Array.prototype.sum = function() {
+  if(this.length !== -1){
+    return this.reduce((a,b) => { return a+ b}, 0)
+  }
+   return 0;
+}
+
+
+//another solution
+Array.range = function(start, count) {
+  return Array.apply(null, Array(count)).map(function(e, i) {
+    return start + i;
+  });
+}
+
+Array.prototype.sum = function() {
+  return this.reduce(function(prev, next) {
+    return prev + next;
+  }, 0);
+}
