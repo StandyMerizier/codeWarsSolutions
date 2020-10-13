971866c595ba29044b07a85369e0df5896400877
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2020-10-12 21:31:55 -0400

    Create countTheDivisorsOfANumber.js

---
+++ countTheDivisorsOfANumber.js
@@ -0,0 +1,21 @@
+//Count the number of divisors of a positive integer n.
+//
+//Random tests go up to n = 500000.
+//
+//Examples
+//divisors(4)  = 3  // 1, 2, 4
+//divisors(5)  = 2  // 1, 5
+//divisors(12) = 6  // 1, 2, 3, 4, 6, 12
+//divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
+
+
+//solution
+function getDivisorsCnt(n){
+  let count = 0
+  for(let i=1; i<=n; i++){
+    if(n%i===0) {
+      count = count+1
+    }
+  }
+    return count
+}
