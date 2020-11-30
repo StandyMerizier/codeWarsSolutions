3be37d2758fe69c725cdf8be15b7b312bd9a83ea
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2020-11-29 20:03:49 -0500

    Create pyramidArray.js

---
+++ pyramidArray.js
@@ -0,0 +1,22 @@
+//Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+//
+//pyramid(0) => [ ]
+//pyramid(1) => [ [1] ]
+//pyramid(2) => [ [1], [1, 1] ]
+//pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+//Note: the subarrays should be filled with 1s
+
+
+//solution
+function pyramid(n) {
+  let res = [];
+  for(let i = 0; i < n; i++)
+    res.push([...Array(i+1)].fill(1));
+  return res;
+}
+
+
+//another solution
+function pyramid(n) {
+  return Array.from({length: n}, (_, i) => Array(i+1).fill(1));
+}
