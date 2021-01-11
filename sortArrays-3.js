78a1823c7d89fcaae847907f464d46cfd4453e7d
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2021-01-11 14:33:30 -0500

    Create sortArrays-3.js

---
+++ sortArrays-3.js
@@ -0,0 +1,10 @@
+//This time the input is a sequence of course-ids that are formatted in the following way:
+//
+//name-yymm
+//The return of the function shall first be sorted by yymm, then by the name (which varies in length).
+
+
+//solution
+let compare = (a, b) => a.slice(-4) - b.slice(-4) || a.localeCompare(b);
+let sortme = (courses) => courses.sort(compare);
+
