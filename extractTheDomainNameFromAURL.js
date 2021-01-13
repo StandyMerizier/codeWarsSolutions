060e6fa94814ca3ba18fec6373aa52d2deb099e9
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2021-01-13 13:54:35 -0500

    Create extractTheDomainNameFromAURL.js

---
+++ extractTheDomainNameFromAURL.js
@@ -0,0 +1,22 @@
+//Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+//
+//domainName("http://github.com/carbonfive/raygun") == "github" 
+//domainName("http://www.zombie-bites.com") == "zombie-bites"
+//domainName("https://www.cnet.com") == "cnet"
+
+
+//solution
+let domainName = function(url) {
+  let match = url.match(/(https?\:\/\/)?([w]{0,3}\.)?([^\.]+)/)
+  return match[match.length-1];
+}
+
+
+//another solution
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
+
