32c43306e737b4bc847d8faa923f29cb6f285d77
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2021-02-08 14:53:41 -0500

    Create searchInMultidimensionalArray.js

---
+++ searchInMultidimensionalArray.js
@@ -0,0 +1,14 @@
+//Write a function that gets a sequence and value and returns true/false depending on whether the variable exists in a multidimentional sequence.
+//
+//Example:
+//
+//locate(['a','b',['c','d',['e']]],'e'); // should return true
+//locate(['a','b',['c','d',['e']]],'a'); // should return true
+//locate(['a','b',['c','d',['e']]],'f'); // should return false
+
+
+//solution
+var locate = function(arr, v) {
+  return arr.some(function(e) { return Array.isArray(e) ? locate(e, v) : e === v; });
+}
+
