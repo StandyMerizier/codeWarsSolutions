ac8e03ddcee3d25cbc2ec1f180690795fa21cdf8
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2021-09-10 23:44:41 -0400

    Create domainType.js

---
+++ domainType.js
@@ -0,0 +1,56 @@
+// GoDaddy makes a lot of different top-level domains available to its customers. A top-level domain is one that goes directly after the last dot ('.') in the domain name, for example .com in example.com. To help the users choose from available domains, GoDaddy is introducing a new feature that shows the type of the chosen top-level domain. You have to implement this feature.
+// To begin with, you want to write a function that labels the domains as "commercial", "organization", "network" or "information" for .com, .org, .net or .info respectively.
+// For the given list of domains return the list of their labels.
+
+// Example
+
+// For domains = ["en.wiki.org", "codesignal.com", "happy.net", "code.info"], the output should be
+// domainType(domains) = ["organization", "commercial", "network", "information"].
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.string domains
+
+// A list of domains, where each domain contains at least one dot. It is guaranteed that each top-level domain of these domains belongs to one of the types described above.
+
+// Guaranteed constraints:
+// 4 ≤ domains.length ≤ 25,
+// 5 ≤ domains[i].length ≤ 20.
+
+// [output] array.string
+
+// The list of labels for the given domains.
+
+
+//solution
+function domainType(domains) {
+    let result = []
+    for(let i = 0; i < domains.length; i++){
+        if(domains[i].substring(domains[i].length - 3) === "com"){
+            result.push("commercial")
+        }
+        else if(domains[i].substring(domains[i].length - 3) === "org"){
+            result.push("organization")
+        }
+        else if(domains[i].substring(domains[i].length - 3) === "net"){
+            result.push("network")
+        }
+        else if(domains[i].substring(domains[i].length - 4) === "info"){
+            result.push("information")
+        }
+    }
+    
+    return result
+}
+
+
+//another solution
+g = 'organization'
+m = 'commercial'
+t = 'network'
+o = 'information'
+domainType = d => 
+    d.map(x => eval([...x].pop()))
+
