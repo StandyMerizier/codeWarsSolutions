66cdc2f8a9e17007e0fb9929dab9a3684a6ff962
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2023-02-13 19:48:39 -0500

    Create sumOfCubes.js

---
+++ sumOfCubes.js
@@ -0,0 +1,21 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// Examples: (Input --> output)
+
+// 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+// 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+
+
+//solution
+function sumCubes(n){
+  if (n === 1) return 1
+  
+  return Math.pow(n, 3) + sumCubes(n-1)
+}
+
+//another solution
+function sumCubes(n) {
+  return (n * (n + 1) / 2) ** 2;
+}
