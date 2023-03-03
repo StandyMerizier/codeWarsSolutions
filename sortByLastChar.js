3e12fcb4169491fed0427037156cabe28812d118
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2023-03-02 20:37:19 -0500

    Create sortByLastChar.js

---
+++ sortByLastChar.js
@@ -0,0 +1,15 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+
+//solution
+const last = x => x.split(' ').sort((a, b) => a.slice(-1).localeCompare(b.slice(-1)))
+
+
+//another solution
+function last(x){
+  return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
