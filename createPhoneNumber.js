de4eb3958c5f3caa742f3fdb42fe3fdd30ded940
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2023-04-04 19:22:46 -0400

    Update createPhoneNumber.js

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -32,3 +32,8 @@ function createPhoneNumber(numbers){
   return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
 }
 
+
+function createPhoneNumber(numbers){
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
