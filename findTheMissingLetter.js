42d6252e4022355e81c649e0b0b9df25de6ee53c
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2023-05-15 20:45:02 -0400

    Create findTheMissingLetter.js

---
+++ findTheMissingLetter.js
@@ -0,0 +1,44 @@
+// Find the missing letter
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+// (Use the English alphabet with 26 letters!)
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+
+//solution
+function findMissingLetter(array)
+{
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  let j = alphabet.indexOf(array[0])
+  
+  for(let letter of array){
+    if(letter === alphabet[j]){
+      j++
+    }
+    else break
+  }
+  
+  return alphabet[j];
+}
+
+
+//another solution
+function findMissingLetter(array) {
+  let first = array[0].charCodeAt(0)
+  for (let i = 1; i < array.length; i++) {
+    if (first + i !== array[i].charCodeAt(0)) {
+      return String.fromCharCode(first + i)
+    }
+  }
+  throw new Error("Invalid input")
+}
