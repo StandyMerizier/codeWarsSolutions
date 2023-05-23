7f9c283e74df1f0abd2b74413d4787f702393d7b
Standy Merizier <39638289+StandyMerizier@users.noreply.github.com>
2023-05-22 20:01:18 -0400

    Update arrayHelpers.js

--- arrayHelpers.js
+++ arrayHelpers.js
@@ -37,4 +37,3 @@ Array.prototype.average = function () { return this.sum() / this.length; }
 Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
 Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
 Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
-
